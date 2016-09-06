141c42cc9c1333ad0e87f309d51f9d82c1ae8149
Runkai Zheng <838474413@qq.com>
2016-09-06 11:49:25 +0800

    DMA Config

--- User/adc.c
+++ User/adc.c
@@ -5,6 +5,7 @@
 
 void ADC1_Init()
 {	
+		DMA_InitTypeDef DMA_InitStructure;
 		GPIO_InitTypeDef GPIO_InitStructure;
 
 		// GPIO≈‰÷√
@@ -13,4 +14,20 @@ void ADC1_Init()
 		GPIO_InitStructure.GPIO_Pin=GPIO_Pin_0;
 		GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AIN;
 		GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+		// DMA≈‰÷√
+		DMA_DeInit(DMA1_Channel1);
+		DMA_InitStructure.DMA_PeripheralBaseAddr=ADC1_DR_Address;
+		DMA_InitStructure.DMA_MemoryBaseAddr=(u32)&ADC_ConvertedValue;
+		DMA_InitStructure.DMA_DIR=DMA_DIR_PeripheralSRC;
+		DMA_InitStructure.DMA_BufferSize=1;
+		DMA_InitStructure.DMA_PeripheralInc=DMA_PeripheralInc_Disable;
+		DMA_InitStructure.DMA_MemoryInc=DMA_MemoryInc_Disable;
+		DMA_InitStructure.DMA_PeripheralDataSize=DMA_PeripheralDataSize_HalfWord;
+		DMA_InitStructure.DMA_MemoryDataSize=DMA_MemoryDataSize_HalfWord;
+		DMA_InitStructure.DMA_Mode=DMA_Mode_Circular;
+		DMA_InitStructure.DMA_Priority=DMA_Priority_High;
+		DMA_InitStructure.DMA_M2M=DMA_M2M_Disable;
+		DMA_Init(DMA1_Channel1,&DMA_InitStructure);
+		DMA_Cmd(DMA1_Channel1,ENABLE);
 }	
