eab78524d36dcf90f617f75b70cf693af26d14fc
Runkai Zheng <838474413@qq.com>
2016-09-06 11:49:53 +0800

    ADC Config

--- User/adc.c
+++ User/adc.c
@@ -6,6 +6,7 @@
 void ADC1_Init()
 {	
 		DMA_InitTypeDef DMA_InitStructure;
+		ADC_InitTypeDef ADC_InitStructure;
 		GPIO_InitTypeDef GPIO_InitStructure;
 
 		// GPIO≈‰÷√
@@ -30,4 +31,22 @@ void ADC1_Init()
 		DMA_InitStructure.DMA_M2M=DMA_M2M_Disable;
 		DMA_Init(DMA1_Channel1,&DMA_InitStructure);
 		DMA_Cmd(DMA1_Channel1,ENABLE);
+
+		// ADC≈‰÷√
+		ADC_InitStructure.ADC_Mode=ADC_Mode_Independent;
+		ADC_InitStructure.ADC_ScanConvMode=DISABLE;
+		ADC_InitStructure.ADC_ContinuousConvMode=ENABLE;
+		ADC_InitStructure.ADC_ExternalTrigConv=ADC_ExternalTrigConv_None;
+		ADC_InitStructure.ADC_DataAlign=ADC_DataAlign_Right;
+		ADC_InitStructure.ADC_NbrOfChannel=0;
+		ADC_Init(ADC1,&ADC_InitStructure);
+		RCC_ADCCLKConfig(RCC_PCLK2_Div8);
+		ADC_RegularChannelConfig(ADC1,ADC_Channel_0,1,ADC_SampleTime_55Cycles5);
+		ADC_DMACmd(ADC1,ENABLE);
+		ADC_Cmd(ADC1,ENABLE);
+		ADC_ResetCalibration(ADC1);
+		while(ADC_GetResetCalibrationStatus(ADC1));
+		ADC_StartCalibration(ADC1);
+		while(ADC_GetCalibrationStatus(ADC1));
+		ADC_SoftwareStartConvCmd(ADC1,ENABLE);
 }	
