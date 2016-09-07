a7477f33ab64288c92c20acdf8ecf231ae4dec5e
Runkai Zheng <838474413@qq.com>
2016-09-07 11:53:59 +0800

    IIC Config

---
+++ User/stm32_iic.c
@@ -0,0 +1,9 @@
+#include "stm32_iic.h"
+
+#define  SCL_H         GPIOB->BSRR = GPIO_Pin_6  
+#define  SCL_L         GPIOB->BRR  = GPIO_Pin_6  
+#define  SDA_H         GPIOB->BSRR = GPIO_Pin_7  
+#define  SDA_L         GPIOB->BRR  = GPIO_Pin_7 
+
+#define  SCL_read      GPIOB->IDR  & GPIO_Pin_6  
+#define  SDA_read      GPIOB->IDR  & GPIO_Pin_7  
