36af0f2ce995fc783e8d031389d48a0bf5929440
Runkai Zheng <838474413@qq.com>
2016-09-09 12:04:33 +0800

    IIC GPIO init

--- User/stm32_iic.c
+++ User/stm32_iic.c
@@ -16,3 +16,16 @@ static void I2C_delay(void)
 
     i--;
 }
+
+//IIC接口初始化
+void I2CInit()
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+ 
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
