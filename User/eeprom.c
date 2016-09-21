4e2aecc6eca2281e38a9db4838d5e7a0251841f1
Runkai Zheng <838474413@qq.com>
2016-09-21 12:58:58 +0800

    Write to flash

--- User/eeprom.c
+++ User/eeprom.c
@@ -15,3 +15,28 @@ void delay_ms(u32 n)
 
 // 设置EEPROM的大小为1K
 u16 STMFLASH_BUFF[STMFLASH_SIZE];
+
+void STMFLASH_Write(u16 WriteAddr,u16 *pBuffer,u16 NumToWrite)
+{
+		u16 j;
+		// 读出备份区域的数据
+		STMFLASH_Read_Backup();
+	
+		// 解锁
+		FLASH_Unlock();
+	
+		// 擦除第255页
+		FLASH_ErasePage(FLASH_PAGE255_ADDR);
+	
+		// 将数据写到数组中去
+		for(j=0;j<NumToWrite;j++)
+		{
+				STMFLASH_BUFF[j+WriteAddr] = *(pBuffer+j);
+		}	
+		
+		// 将数据写回到第255页中
+		STMFLASH_Write_NoErase();
+		
+		// 上锁	
+		FLASH_Lock();
+}
